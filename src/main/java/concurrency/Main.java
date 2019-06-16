e91b0d080b454dc02b3143a8d84976d3d4bedb03
ater <alexandr.ter@endava.com>
2019-06-16 11:45:06 +0300

    Synchronized Method, multiple execution

--- src/main/java/concurrency/Main.java
+++ src/main/java/concurrency/Main.java
@@ -2,7 +2,7 @@ package concurrency;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main() {
         Runnable target = new DownloadingService();
 
         Thread iPhone = new Thread(target, "iPhone");
@@ -11,6 +11,13 @@ public class Main {
         iPhone.start();
         iPad.start();
 
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true){
+            main();
+            Thread.sleep(1000);
+        }
     }
 
 }
