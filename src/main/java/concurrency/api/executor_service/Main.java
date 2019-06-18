8143358bc07291ef9815b848deca7af21d559615
ater <alexandr.ter@endava.com>
2019-06-18 06:35:56 +0300

    executorService shutDown()

--- src/main/java/concurrency/api/executor_service/Main.java
+++ src/main/java/concurrency/api/executor_service/Main.java
@@ -6,33 +6,33 @@ import java.util.concurrent.Future;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception /*TODO 2 try to remove "throws Exception"*/{
+    public static void main(String[] args) throws Exception{
 
-        ExecutorService executorService = Executors.newSingleThreadExecutor(); //TODO 3 try to use another implementation
+        ExecutorService executorService = null;
+        Future<String> feature;
 
-        System.out.println("main thread is going to submit some tasks to executor service \n");
+        System.out.println("main thread is going to submit one task to executor service \n");
 
-        final Future<?> feature1 =
-                executorService.submit(() -> System.out.println(Thread.currentThread().getName() +
-                        "   1.st task executing by executor service"));
+        try{
+            executorService = Executors.newSingleThreadExecutor();
+            feature =
+                    executorService.submit(() -> {
+                        System.out.println("\n1.st task executing by executor service");
+                        Thread.sleep(5000);
+                        return "some result";
+                    });
+        } finally {
+            if (executorService != null) {
+                executorService.shutdown();
+                System.out.println("executorService shut down? " + executorService.isShutdown());
+                System.out.println("executorService terminated? " + executorService.isTerminated());
+            }
+        }
 
-        final Future<String> feature2 =
-                executorService.submit(() -> {
-                    System.out.println(Thread.currentThread().getName() +
-                            "   2.nd task executing by executor service");
-                    Thread.sleep(10000);
-                    return "some result";
-                });
+        System.out.println("\n1.st task done? = " + feature.isDone());
+        System.out.println("1.st task's result = " + feature.get());
+        System.out.println("1.st task done? = " + feature.isDone());
 
-        System.out.println("\n1.st task done? = " + feature1.isDone());
-        System.out.println("1.st task's result = " + feature1.get());
-
-        System.out.println("\n2.nd task done? = " + feature2.isDone());
-        System.out.println("2.nd task's result = " + feature2.get());//TODO 1 try to add timeout
-        System.out.println("2.nd task done? = " + feature2.isDone());
-        System.out.println("2.nd task canceled? = " + feature2.isCancelled());
-
-        System.out.println("\nmain thread is done");
+        System.out.println("\nexecutorService terminated? " + executorService.isTerminated());
     }
 }
-//TODO 4, revert changes, go to the next commit
